fb090d7749fd5cb0a265f574e6d48f3c7d2ad72a
ADESH MAHTO <adeshmahto86@gmail.com>
2025-11-04 09:51:00 +0530

    add patient entity

---
+++ Patient Management Microservice/Patient-Management-Microservice/src/main/java/com/patientmanagement/Patient/Management/Microservice/model/Patient.java
@@ -0,0 +1,39 @@
+package com.patientmanagement.Patient.Management.Microservice.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+}
